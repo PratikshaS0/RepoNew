21baa335aa1296ee75073ec133e80c36da6e88e9
PratikshaS0 <pratikshas076@gmail.com>
2022-03-30 22:35:09 +0530

    First commit by X

---
+++ Demo/src/test/Test2.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+	String test = "Welcome to java";
+	if(test.contains("java"))
+	{
+		System.out.println("Found !!!!");
+	}
+
+	}
+
+}
